bd4b964fa95cf501eb64b964a23f12485a68b98d
Santiago Bruno <santibrunovm@gmail.com>
2022-10-24 11:09:56 -0300

    agregando clases

--- Clase4-ejercicioExtra/src/main/java/Main.java
+++ Clase4-ejercicioExtra/src/main/java/Main.java
@@ -5,9 +5,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Documento uno = new Documento("nivel 1", 1);
-        Documento dos = new Documento("nivel 2", 2);
-        Documento tres = new Documento("nivel 3", 3);
+        Documento uno = new Documento("doc nivel 1", 1);
+        Documento dos = new Documento("doc nivel 2", 2);
+        Documento tres = new Documento("doc nivel 3", 3);
 
         Handler handler = new Handler();
 
