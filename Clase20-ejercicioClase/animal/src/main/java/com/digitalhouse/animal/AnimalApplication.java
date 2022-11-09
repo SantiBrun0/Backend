3aceeb8ca1a5a29e809c4488faaea2a33ef5bff0
Santiago Bruno <santibrunovm@gmail.com>
2022-11-09 13:36:13 -0300

    agregando clases

---
+++ Clase20-ejercicioClase/animal/src/main/java/com/digitalhouse/animal/AnimalApplication.java
@@ -0,0 +1,13 @@
+package com.digitalhouse.animal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalApplication.class, args);
+	}
+
+}
